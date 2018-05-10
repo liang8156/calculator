aba1bc694c538e6171a3c0e3017851fbad01112d
alexLiang <liang8156@gmail.com>
2018-05-11 00:49:57 +0800

    .還沒完成

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,31 @@ let calculatorElement = ["AC", "CE", "/", "%",
     "4", "5", "6", "-",
     "1", "2", "3", "+",
     ".", "0", "=", "empty"]
-function runAC() {
-    document.calculator.display.value = "";
-    console.log(calculatorElement)
-};
 
-function runCE() {
-    document.calculator.display.value = document.calculator.display.value.slice(0, -1);
-};
-
-function run(number) {
-    console.log(number)
-    document.calculator.display.value += number;
+function run(symbol) {
+    switch (symbol) {
+        case 'AC':
+            document.calculator.display.value = "";
+            break
+        case 'CE':
+            document.calculator.display.value = document.calculator.display.value.slice(0, -1);
+            break
+        case '=':
+            var Equals = eval(document.calculator.display.value);
+            document.calculator.display.value = Equals;
+            break
+        case '+/-':
+            if (document.calculator.display.value.substr(0, 1) != '-') {
+                document.calculator.display.value = "-" + document.calculator.display.value;
+            }
+            else {
+                document.calculator.display.value = document.calculator.display.value.slice(1, )
+            }
+            break
+        default:
+            document.calculator.display.value += symbol;
+            break
+    }
 };
 
 function runEquals() {
